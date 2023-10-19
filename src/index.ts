60058c7963fd1d8677ba76866bb298c699af9321
Tevin-Isaac <isaactevin@yahoo.com>
2023-10-19 14:54:58 +0300

    updated functions in index.ts file

--- src/index.ts
+++ src/index.ts
@@ -41,14 +41,14 @@ type CollectorPayload = Record<{
     collectorName: string;
 }>
 
-// Define QuotePayload for creator to upload/update 
+// Define QuotePayload for creator to upload/update
 type QuotePayload = Record<{
     content: string;
     collectorId: string;
     authorId: string;
 }>
 
-// Create a new StableBTreeMap to store authors, collectors and quotes
+// Create a new StableBTreeMap to store authors, collectors, and quotes
 const collectorStorage = new StableBTreeMap<string, Collector>(0, 44, 1024);
 const authorStorage = new StableBTreeMap<string, Author>(1, 44, 1024);
 const quoteStorage = new StableBTreeMap<string, Quote>(2, 44, 1024);
@@ -64,7 +64,7 @@ $query;
 export function getAuthor(id: string): Result<Author, string> {
     return match(authorStorage.get(id), {
         Some: (author) => Result.Ok<Author, string>(author),
-        None: () => Result.Err<Author, string>(`author with id=${id} not found`)
+        None: () => Result.Err<Author, string>(`Author with id=${id} not found`)
     });
 }
 
@@ -79,7 +79,7 @@ $query;
 export function getCollector(id: string): Result<Collector, string> {
     return match(collectorStorage.get(id), {
         Some: (collector) => Result.Ok<Collector, string>(collector),
-        None: () => Result.Err<Collector, string>(`collector with id=${id} not found`)
+        None: () => Result.Err<Collector, string>(`Collector with id=${id} not found`)
     });
 }
 
@@ -94,7 +94,7 @@ $query;
 export function getQuoteById(id: string): Result<Quote, string> {
     return match(quoteStorage.get(id), {
         Some: (quote) => Result.Ok<Quote, string>(quote),
-        None: () => Result.Err<Quote, string>(`quote with id=${id} not found`)
+        None: () => Result.Err<Quote, string>(`Quote with id=${id} not found`)
     });
 }
 
@@ -183,11 +183,11 @@ export function registerCollector(payload: CollectorPayload): Result<Collector, 
         ...payload,
     };
 
-    // revert if collector already registered
+    // Revert if collector already registered
     const collectors = collectorStorage.values();
     for (const c of collectors) {
         if (c.principal.toString() == ic.caller().toString()) {
-            return Result.Err<Collector, string>(`collector already registered`);
+            return Result.Err<Collector, string>(`Collector already registered`);
         }
     }
 
@@ -226,6 +226,59 @@ export function updateCollector(
     });
 }
 
+$update;
+// Update author's information
+export function updateAuthor(
+    id: string,
+    payload: AuthorPayload
+): Result<Author, string> {
+    return match(authorStorage.get(id), {
+        Some: (author: Author) => {
+            // Check if the caller is authorized to update the author's information
+            if (ic.caller().toString() !== author.principal.toString()) {
+                return Result.Err<Author, string>(
+                    `You are not authorized to update this author's information.`
+                );
+            }
+
+            const updatedAuthor: Author = {
+                ...author,
+                ...payload,
+                updatedAt: Opt.Some(ic.time()), // Set the update timestamp to the current time
+            };
+            authorStorage.insert(author.id, updatedAuthor); // Update the author in the author storage
+            return Result.Ok<Author, string>(updatedAuthor);
+        },
+        None: () => 
+            Result.Err<Author, string>(
+                `Couldn't update an author with id=${id}. Author not found.`
+            ),
+    });
+}
+
+$update;
+// Delete an author by ID
+export function deleteAuthor(id: string): Result<string, string> {
+    return match(authorStorage.get(id), {
+        Some: (author: Author) => {
+            // Check if the caller is authorized to delete the author
+            if (ic.caller().toString() !== author.principal.toString()) {
+                return Result.Err<string, string>(
+                    `You are not authorized to delete this author.`
+                );
+            }
+
+            authorStorage.remove(id); // Remove the author from the author storage
+            return Result.Ok<string, string>(`Author deleted successfully.`);
+        },
+        None: () => {
+            return Result.Err<string, string>(
+                `Couldn't delete an author with id=${id}. Author not found.`
+            );
+        },
+    });
+}
+
 // collector upload quote, failed if author not exist
 $update;
 export function uploadQuote(payload: QuotePayload): Result<Quote, string> {
@@ -249,7 +302,7 @@ export function uploadQuote(payload: QuotePayload): Result<Quote, string> {
 
     // Revert if author not exist
     if (authorId == "") {
-        return Result.Err<Quote, string>(`author with id ${payload.authorId} not found`);
+        return Result.Err<Quote, string>(`Author with id ${payload.authorId} not found`);
     }
 
     quoteStorage.insert(quote.id, quote); // Store the quote in the quote storage
@@ -303,7 +356,7 @@ export function deleteQuote(id: string): Result<string, string> {
       },
       None: () => {
         return Result.Err<string, string>(
-          `couldn't delete a quote with id=${id}. quote not found`
+          `Couldn't delete a quote with id=${id}. Quote not found`
         );
       },
     });
@@ -321,6 +374,67 @@ export function createAuthor(payload: AuthorPayload): Result<Author, string> {
 
     authorStorage.insert(author.id, author); // Store the author in the author storage
     return Result.Ok(author);
+}
+
+$update;
+// Get quotes by collector's principal
+export function getQuotesByCollectorPrincipal(): Result<Vec<Quote>, string> {
+    const quotes = quoteStorage.values();
+    const returnedQuotes: Quote[] = [];
+
+    for (const quote of quotes) {
+        if (quote.collector.principal.toString() == ic.caller().toString()) {
+            returnedQuotes.push(quote);
+        }
+    }
+
+    return Result.Ok(returnedQuotes);
+}
+
+$update;
+// Get all authors and their quotes
+export function getAuthorsWithQuotes(): Result<Vec<Record<Author, Vec<Quote>>>, string> {
+    const authors = authorStorage.values();
+    const authorsWithQuotes: Record<Author, Vec<Quote>>[] = [];
+
+    for (const author of authors) {
+        const authorQuotes = getQuotesByAuthorId(author.id).value;
+        authorsWithQuotes.push({ author, quotes: authorQuotes });
+    }
+
+    return Result.Ok(authorsWithQuotes);
+}
+
+$update;
+// Get the total number of quotes in the storage
+export function getTotalQuoteCount(): Result<number, string> {
+    const quotes = quoteStorage.values();
+    return Result.Ok(quotes.length);
+}
+
+$update;
+// Get the oldest and newest quotes by creation timestamp
+export function getOldestAndNewestQuotes(): Result<Record<Quote, Quote>, string> {
+    const quotes = quoteStorage.values();
+
+    if (quotes.length < 2) {
+        return Result.Err("There are not enough quotes to compare.");
+    }
+
+    quotes.sort((a, b) => a.createdAt - b.createdAt);
+
+    const oldestQuote = quotes[0];
+    const newestQuote = quotes[quotes.length - 1];
+
+    return Result.Ok({ oldestQuote, newestQuote });
+}
+
+$update;
+// Get a list of unique author names
+export function getUniqueAuthorNames(): Result<Vec<string>, string> {
+    const authors = authorStorage.values();
+    const uniqueAuthorNames = Array.from(new Set(authors.map(author => author.name)));
+    return Result.Ok(uniqueAuthorNames);
 }
 
 // a workaround to make uuid package work with Azle
